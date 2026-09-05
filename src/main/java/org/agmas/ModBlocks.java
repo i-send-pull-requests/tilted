7049618b5288a3767f50598bf54df4b9f3ebfb33
i-send-pull-requests <i-send-pull-requests@proton.me>
2026-09-05 18:22:56 +0800

    added crate to creative tab

--- src/main/java/org/agmas/ModBlocks.java
+++ src/main/java/org/agmas/ModBlocks.java
@@ -1,10 +1,12 @@
 package org.agmas;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -22,6 +24,14 @@ public class ModBlocks {
     );
 
     public static void init() {
+        //? if >=26.1 {
+        /*CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS)
+         *///? } else {
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS)
+        //? }
+            .register((tab) -> {
+                tab.accept(CRATE.asItem());
+            });
     }
 
     private static Block register(String name, Function<BlockBehaviour.Properties, Block> blockFactory, BlockBehaviour.Properties properties, boolean shouldRegisterItem) {
